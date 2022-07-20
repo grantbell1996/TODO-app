05f9d8ecbe2cdbfb5ba11dfbddb98f6cfdc56784
Grant Bell <grantjbell@gmail.com>
2022-07-20 16:51:13 -0500

    connected to firebase db

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Todo from './Todo';
 import "./App.css";
+import db from './firebase'
+import firebase from "firebase/compat/app";
+
 
 function App() {
-  const [todos, setTodos] = useState(["take dogs for a walk", "go to jim"]);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  useEffect(() => {
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      //snapshot is taking a "snapshot" of the database, then mapping through the docs (the individual objects in the database)
+      //then grabbing the objects with the key "todo" and setting to the todo array in the above useState
+      console.log(snapshot.docs.map(doc => doc.data()))
+      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    })
+  }, []);
 
   const addTodo = (event) => {
     event.preventDefault(); //stops the page from refreshing so we dont lose the current state
-    setTodos([...todos, input]); //the array includes a spread of the current state of todos and pushes the value of the input field to the current array
+
+    db.collection('todos').add({
+      todo: input, 
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }) //because you passed 'todos' to db.collection in the above useeffect, a snapshot of the db is now triggered when the input is changed
+
+
     setInput(''); //setting the input back to an empty string to clear the form
   };
 
   return (
     <div className="App">
       <h1>hello me</h1>
+      
       <form>
         <input
         placeholder="write a todo"
@@ -24,12 +44,12 @@ function App() {
       </form>
 
       <ul>
-        {todos.map((todo) => (
-          <li>{todo}</li>
+        {todos.map(todo => (
+          <Todo text={todo}/>
+          // <li>{todo}</li>
         ))}
-        <li></li>
-        <li></li>
       </ul>
+
     </div>
   );
 }
